0556aa255e1b336aa530c864121094f9a5c5f485
Akash1012 <gupta.akash2090@gmail.com>
2021-06-12 22:28:15 +0530

    Wrap error module

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import { getEventById } from "../../dummy-data";
+import ErrorAlert from "../../components/UI/error-alert";
 
 const EventDetailPage = () => {
   const router = useRouter();
@@ -11,7 +12,11 @@ const EventDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event Found</p>;
+    return (
+      <ErrorAlert>
+        <p>No Event Found</p>
+      </ErrorAlert>
+    );
   }
 
   return (
