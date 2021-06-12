64d345fd146695758f03fa83039f05763ccb5dc9
Akash1012 <gupta.akash2090@gmail.com>
2021-06-12 22:22:19 +0530

    Complete route module

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/event/event-list";
+import EventsSearch from "../../components/event/events-search";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+  const findEventHandler = (year, month) => {
+    const fullpath = `/events/${year}/${month}`;
+    router.push(fullpath);
+  };
+
   return (
-    <div>
+    <React.Fragment>
+      <EventsSearch onSearch={findEventHandler} />
       <EventList items={events} />
-    </div>
+    </React.Fragment>
   );
 };
 export default AllEventsPage;
